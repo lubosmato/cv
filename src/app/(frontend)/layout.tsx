e7f89fe914a2e648a49b69b3c2c2485416eae1f9
lubosmato <lubos.matejcik@gmail.com>
2025-03-02 00:46:21 +0100

    feat: add OG tags

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,9 +1,47 @@
+import { Metadata } from 'next'
 import React from 'react'
-import './styles.css'
 
-export const metadata = {
-  description: "Software engineer passionate about technology, innovation, and software development.",
-  title: 'Curriculum vitae Luboš Matejčík – Software Engineer',
+import './styles.css'
+import { getPayload } from '@/lib/payload'
+
+export async function generateMetadata(): Promise<Metadata> {
+  const payload = await getPayload()
+  const profile = await payload.findGlobal({ slug: "profile", depth: 1 })
+  const profileImageUrl = (typeof profile.photo !== "number" ? profile.photo.url : null) ?? null
+
+  return {
+    title: `Curriculum vitae ${profile.name} - ${profile.profession}`,
+    description: "Software engineer passionate about technology, innovation, and software development.",
+    authors: [{ name: profile.name }],
+    creator: profile.name,
+    publisher: profile.name,
+    formatDetection: {
+      email: true,
+      address: true,
+      telephone: true,
+    },
+    openGraph: {
+      title: `Curriculum vitae ${profile.name} - ${profile.profession}`,
+      description: "Software engineer passionate about technology, innovation, and software development.",
+      siteName: `${profile.name} - ${profile.profession}`,
+      locale: 'en_US',
+      type: 'profile',
+      images: profileImageUrl ? [
+        {
+          url: profileImageUrl,
+          width: 512,
+          height: 512,
+          alt: `${profile.name} - ${profile.profession}`
+        }
+      ] : []
+    },
+    robots: {
+      index: false,
+      follow: false,
+    },
+    themeColor: '#c0351f',
+    viewport: 'width=device-width, initial-scale=1',
+  }
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
