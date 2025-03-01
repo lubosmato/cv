33847e21f752f0730344cf41de86435655ae4db2
lubosmato <lubos.matejcik@gmail.com>
2025-03-02 00:08:17 +0100

    feat: update SEO

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './styles.css'
 
 export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  description: "Software engineer passionate about technology, innovation, and software development.",
+  title: 'Curriculum vitae Luboš Matejčík – Software Engineer',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
